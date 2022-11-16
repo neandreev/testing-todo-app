dd32973d2adc8ca5b723bbc67577042f2143f552
Danila Andreev <mail@neandreev.ru>
2022-11-16 22:32:13 +0500

    Fix source code by eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,54 @@
-import { useState } from 'react'
-import { initializeApp } from "firebase/app";
-import reactLogo from './assets/react.svg'
-import './App.less'
+import { useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import reactLogo from './assets/react.svg';
+import './App.less';
+import Todo from './components/todo';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "testing-todo-app-a1e1d.firebaseapp.com",
-  projectId: "testing-todo-app-a1e1d",
-  storageBucket: "testing-todo-app-a1e1d.appspot.com",
-  messagingSenderId: "970510945239",
-  appId: "1:970510945239:web:53a6b72774d883f84d1992"
+  authDomain: 'testing-todo-app-a1e1d.firebaseapp.com',
+  projectId: 'testing-todo-app-a1e1d',
+  storageBucket: 'testing-todo-app-a1e1d.appspot.com',
+  messagingSenderId: '970510945239',
+  appId: '1:970510945239:web:53a6b72774d883f84d1992',
 };
 
 const app = initializeApp(firebaseConfig);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Todo />
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
+          count is
+          {' '}
+          {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.less'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.less';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
