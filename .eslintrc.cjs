ee572caac9b84487e814efac6a267e27d0f76c4c
Danila Andreev <mail@neandreev.ru>
2022-11-22 14:24:37 +0500

    Update eslint rules exceptions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,5 +25,11 @@ module.exports = {
   rules: {
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-autofocus': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'import/no-cycle': 'off',
+    'react/require-default-props': 'off',
   },
 };
